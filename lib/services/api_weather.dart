1f102d4c4f5573857e8a74e5546b49e4eb4eb5c3
Danil Gorbunov <35400605+DanilGorbunov@users.noreply.github.com>
2022-09-15 23:33:39 +0300

    Update api_weather.dart

--- lib/services/api_weather.dart
+++ lib/services/api_weather.dart
@@ -6,7 +6,7 @@ import 'package:theweather/model/weather_model.dart';
 class WeatherApiClient {
   Future<Weather>? getCurrentWeather(String? location) async {
     var endpoint = Uri.parse(
-        "https://api.openweathermap.org/data/2.5/weather?q=$location&appid=66a671dd610594d47547e8aaf1887785&units=metric");
+        "https://api.openweathermap.org/data/2.5/weather?q=$location&appid=add-here-your-api_key-insted-this-text&units=metric");
     var response = await http.get(endpoint);
     var body = jsonDecode(response.body);
     print(Weather.fromJson(body));
